e8d404bc00c0bd277414d14b90194f2e8aa7c871
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-03-08 16:59:49 +0530

    Made the cars to follow the check points

---
+++ AritificalIntelligence/MainProject/Aritfical Intelligence/Assets/Scripts/FollowWP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWP : MonoBehaviour
+{
+    // public variables
+    public GameObject[] waypoints;
+    public float speed = 5f;
+    public float turningSpeed = 5f;
+
+    // private variables
+    int currentWP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWP = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(waypoints[currentWP].transform.position, this.transform.position) < 3)
+            if (currentWP == (waypoints.Length-1))
+                currentWP = 0;
+            else 
+                ++currentWP;
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+        // this.transform.LookAt(waypoints[currentWP].transform.position);
+        Quaternion lookAtWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAtWP, turningSpeed * Time.deltaTime);
+    }
+}
