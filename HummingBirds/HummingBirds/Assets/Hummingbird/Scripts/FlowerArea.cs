3419ffa79009366c17e3a13db4aa57d75d1950f9
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-06-01 00:14:46 +0530

    Added the functions to get all flowers and their componets as a list and a dictionary

--- HummingBirds/HummingBirds/Assets/Hummingbird/Scripts/FlowerArea.cs
+++ HummingBirds/HummingBirds/Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -1,18 +1,117 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages a collection of flower planets and attached flowers
+/// </summary>
 public class FlowerArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // The diameter of the area where the agent and flowers can be used 
+    // for observing relative distance from agent to flower
+    public const float areaDiameter = 20f;
+
+    // The list of all flower plants in this flower area (flower plants have multiple flowers)
+    private List<GameObject> flowerPlants;
+
+    // A lookup dictionary for looking up a flower from a nectar collider
+    private Dictionary<Collider, Flower> nectarFlowerDictionary;
+
+    /// <summary>
+    /// The list of all the flowers in the flower area
+    /// </summary>
+    /// <value>A list of all flowers</value>
+    public List<Flower> Flowers { get; private set; }
+
+    /// <summary>
+    /// Reset the flowers and flower planets
+    /// </summary>
+    public void ResetFlowers()
     {
-        
+        // Rotate each flower plant around the Y axis and subtly around the X and Z axis
+        foreach (GameObject flowerPlant in flowerPlants)
+        {
+            float xRotation = UnityEngine.Random.Range(-5f, 5f);
+            float YRotation = UnityEngine.Random.Range(-180f, 180f);
+            float zRotation = UnityEngine.Random.Range(-5f, 5f);
+        }
+
+        // Resets each flower
+        foreach (Flower flower in Flowers)
+        {
+            flower.ResetFlower();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Gets the <see cref="Flower"/> that a nectar collider belong
+    /// </summary>
+    /// <param name="collider">The nectar collider</param>
+    /// <returns>The matching flower</returns>
+    public Flower GetFlowerFromNectar(Collider collider)
     {
-        
+        return nectarFlowerDictionary[collider];
+    }
+
+    /// <summary>
+    /// Called when the area wakes up
+    /// </summary>
+    private void Awake()
+    {
+        // Initailize variables
+        flowerPlants = new List<GameObject>();
+        nectarFlowerDictionary = new Dictionary<Collider, Flower>();
+        Flowers = new List<Flower>();
+    }
+
+    /// <summary>
+    /// Called when the game starts
+    /// </summary>
+    private void Start()
+    {
+        // Find all flowers that are children of this gameobject/Tranform
+        FindChildFlowers(this.transform);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="transform"></param>
+    private void FindChildFlowers(Transform parent)
+    {
+        for (int i = 0; i < parent.childCount; ++i)
+        {
+            Transform child = parent.GetChild(i);
+
+            if (child.CompareTag("flower_plant"))
+            {
+                // Found a flower plant, add it to the flower plant list
+                flowerPlants.Add(child.gameObject);
+
+                // Look for flowers within the flower component
+                FindChildFlowers(child);
+            }
+            else
+            {
+                // Not a flower plant, look for a Flower component
+                Flower flower;
+                if (child.TryGetComponent<Flower>(out flower)) 
+                {
+                    // Found a flower, add it to the Flowers list
+                    Flowers.Add(flower);
+
+                    // Add the nectar collider to the lookup dictionary
+                    nectarFlowerDictionary.Add(flower.nectorCollider, flower);
+
+                    // Mote: there are no flowers that are children of other flowers
+                } 
+                else
+                {
+                    // Flower component not found, so check children
+                    FindChildFlowers(child);
+                }
+            }
+        }
     }
 }
