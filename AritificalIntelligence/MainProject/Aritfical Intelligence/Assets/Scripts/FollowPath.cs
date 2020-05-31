d43af76a4cd2ca2d0ca82bda8e81dea857c2a29d
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-05-31 23:27:32 +0530

    Added the game files required to humming bird project

---
+++ AritificalIntelligence/MainProject/Aritfical Intelligence/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    void Start () {
+        wps = wpManager.GetComponent<WPManager> ().waypoints;
+        g = wpManager.GetComponent<WPManager> ().graph;
+        currentNode = wps[0];
+    }
+
+    public void gotToHeli() {
+        g.AStar(currentNode, wps[9]);
+        currentWP = 0;
+    }
+
+    public void goToRuin() {
+        g.AStar(currentNode, wps[12]);
+        currentWP = 0;
+    }
+
+    void LateUpdate () {
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        // the node we are closest to at this moment
+        currentNode = g.getPathPoint(currentWP);
+
+        // if we are close enough to the current waypoint move to next
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, this.transform.position) < accuracy) {
+            currentWP++;
+        }
+
+        // if we are not at the end of the path
+        if (currentWP < g.getPathLength()) {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ HummingBirds/HummingBirds/Assets/Hummingbird/Scripts/Flower.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages a single flower with nectar
+/// </summary>
+public class Flower : MonoBehaviour
+{
+    [Tooltip("The color when the flower is full")]
+    public Color fullFlowerColor = new Color(1f, 0f, .3f);
+
+    [Tooltip("The color when the flower is empty")]
+    public Color emptyFlowerColor = new Color(0.5f, 0f, 1f);
+
+    /// <summary>
+    /// The trigger collider representing the nector
+    /// </summary>
+    [HideInInspector]
+    public Collider nectorCollider;
+
+    // The solid collider representing the flower petals
+    private Collider flowerCollider;
+
+    // The flower's material
+    private Material flowerMaterial;
+
+    /// <summary>
+    /// A vector pointing straight out of the flower
+    /// </summary>
+    /// <value></value>
+    public Vector3 FlowerUpVector
+    {
+        get
+        {
+            return nectorCollider.transform.up;
+        }
+    }
+
+    /// <summary>
+    /// The center position of the nector collider
+    /// </summary>
+    /// <value></value>
+    public Vector3 FlowerCenterPosition
+    {
+        get
+        {
+            return nectorCollider.transform.position;
+        }
+    }
+
+    /// <summary>
+    /// The amount of the nector in the flower
+    /// </summary>
+    /// <value></value>
+    public float NectorAmount { get; private set; }
+
+    /// <summary>
+    /// Weather the flower has nector remaining
+    /// </summary>
+    /// <value></value>
+    public bool HasNector
+    {
+        get
+        {
+            return NectorAmount > 0;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to remove nectar from the flower
+    /// </summary>
+    /// <param name="amount">The amount of nector to remove</param>
+    /// <returns>The actual amount successfully removed</returns>
+    public float Feed(float amount){
+        // Track how much nectar was successfully taken (cannot take more then the available)
+        float nectarTaken = Mathf.Clamp(amount, 0f, NectorAmount);
+
+        // Subtract the nectar
+        NectorAmount -= nectarTaken;
+
+        if (!HasNector) 
+        {
+            // Disable the flower and nectar colliders
+            flowerCollider.gameObject.SetActive(false);
+            nectorCollider.gameObject.SetActive(false);
+
+            // Change the color of the flower
+            flowerMaterial.SetColor("_BaseColor", emptyFlowerColor);
+        }
+
+        // Return the amount of nectar that was taken
+        return nectarTaken;
+    }
+
+    /// <summary>
+    /// Resets the flower
+    /// </summary>
+    public void ResetFlower()
+    {
+        // Refill the nectar amount
+        NectorAmount = 1f;
+
+        // Enable the flower and nectar colliders
+        flowerCollider.gameObject.SetActive(true);
+        nectorCollider.gameObject.SetActive(true);
+
+        // Change the color of the flower to indicate that the flower is full
+        flowerMaterial.SetColor("_BaseColor", fullFlowerColor);
+    }
+
+    /// <summary>
+    /// Called whent he flower wakes up
+    /// </summary>
+    private void Awake() 
+    {
+        // Find the flower's mesh render and get the main material
+        flowerMaterial = this.GetComponent<MeshRenderer>().material;
+
+        // Find the flower and nectar colliders
+        flowerCollider = this.transform.Find("FlowerCollider").GetComponent<Collider>();
+        nectorCollider = this.transform.Find("FlowerNectarCollider").GetComponent<Collider>();
+    }
+}
